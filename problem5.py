e468415f1ca486b839838292dfdc3e9fa7113892
JCho24 <johnc415@deltalearns.ca>
2020-10-01 12:26:41 -0700

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -25,7 +25,11 @@ b = float(b)
 c = input("Enter a value for c: ")
 c = float(c)
 
-if (b**2 - (4 * a * c)) > 0:
+d = ((b**2) - (4 * a * c))
+
+if d == 0:
+    print("the equation can be factored")
+elif ((d**d)%d)==0:
     print("the equation can be factored")
 else:
     print("the equation can not be factored")
